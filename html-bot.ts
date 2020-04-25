24d73270b8e58dae046bb444e9dfb919feadd9e2
Alexsey <tolalur@gmail.com>
2020-04-25 12:05:58 +0300

    html bot

---
+++ html-bot.ts
@@ -0,0 +1,9 @@
+import Telegraf from 'telegraf';
+
+const bot = new Telegraf('414766630:AAFIiCodXtCIFsC36vv9r96obnZw1rhc53c');
+
+bot.on('text', ({ replyWithHTML }) => replyWithHTML(
+    `
+    Aleksey completed a task in <a href="https://app.asana.com/0/1172081950609769/list|Project X">task</a>
+`
+))
