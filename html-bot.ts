96fffc91df3c3314183591d5dabf17bd164687a4
Alexsey <tolalur@gmail.com>
2020-04-25 12:24:29 +0300

    сменил ссылку

--- html-bot.ts
+++ html-bot.ts
@@ -4,7 +4,7 @@ const bot = new Telegraf('414766630:AAFIiCodXtCIFsC36vv9r96obnZw1rhc53c');
 
 bot.on('text', ({ replyWithHTML }) => replyWithHTML(
     `
-    Aleksey completed a task in <a href="https://app.asana.com/0/1172081950609769/list|Project X">task</a>
+    Aleksey completed a task in <a href="https:\/\/app.asana.com\/0\/0\/1172815791483369\/f">task</a>
 `
 ));
 bot.launch();
