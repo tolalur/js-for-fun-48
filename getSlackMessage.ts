e98cf66554e34555eedf532d1fc6e058dc88c967
Alexsey <tolalur@gmail.com>
2020-04-30 00:29:53 +0300

    фикс вывода сообщения

--- getSlackMessage.ts
+++ getSlackMessage.ts
@@ -37,7 +37,7 @@ const main = async () => {
         // telegram.sendMessage('-1001498144190')
     } else {
         console.log("Не удалось загрузить сообщения");
-        telegram.sendMessage('-1001498144190', 'Похоже пидоры из слака, заблочили интеграцию...' + messages.toString())
+        telegram.sendMessage('-1001498144190', 'Похоже пидоры из слака, заблочили интеграцию... ' + JSON.stringify(messages))
     }
 };
 
